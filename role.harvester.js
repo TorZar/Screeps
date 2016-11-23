87c0c888f349b670063fe844da3c813bac299de5
TorZar <tonyrfoxjr@gmail.com>
2016-11-23 14:32:44 -0500

    Switch order of for larger carry

--- role.harvester.js
+++ role.harvester.js
@@ -3,22 +3,13 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-
-            /*  ### CODE TO HARVEST ANY FREE SOURCE ###  */
-
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        } else {
+        if (creep.carry.energy != 0) {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity ||
-                            structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity ||
-                            structure.structureType == STRUCTURE_ROAD && structure.energy < structure.energyCapacity ||
-                            structure.structureType == STRUCTURE_CONTAINER && structure.energy < structure.energyCapacity);
+                        structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity ||
+                        structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity ||
+                        structure.structureType == STRUCTURE_STORAGE && structure.energy < structure.energyCapacity);
                 }
             });
             if (targets.length > 0) {
@@ -28,12 +19,20 @@ var roleHarvester = {
                 }
 
             } else {
-
                 creep.moveTo(Game.spawns['Spawn1']);
                 if (varConstants.SPEAKABLE) {
                     creep.say('Homing...');
                 };
             };
+
+        } else {
+            var sources = creep.room.find(FIND_SOURCES);
+
+            /*  ### CODE TO HARVEST ANY FREE SOURCE ###  */
+
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
         }
     }
 };
